f381efd6a5f68f0c71e693f0260f8fb6e0ab3d51
Constanta Mazur <mazur.constanta@gmail.com>
2023-04-18 05:00:05 +0300

    fixed 1st homeword

--- L1_Calculator/src/homework1/CalculationOperations.java
+++ L1_Calculator/src/homework1/CalculationOperations.java
@@ -5,12 +5,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CalculationOperations {
-    private double firstNum;
-    private double secondNum;
-    private double result;
+    private Double firstNum;
+    private Double secondNum;
+    private Double result;
     private char operation;
 
-    Scanner scanner = new Scanner(System.in);
 
     public double getFirstNum() {
         return firstNum;
@@ -65,49 +64,81 @@ public class CalculationOperations {
         return result = firstNum / secondNum;
     }
 
-    public String performCalculation() {
-        enterNumbers();
-        switch(chooseOperation()) {
-            case '+':
-                result = sum(firstNum, secondNum);
+    public void performCalculation() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            boolean numbers = enterNumbers();
+            while (numbers) {
+                numbers = enterNumbers();
+            }
+
+            boolean operator = chooseOperation();
+            while (operator) {
+                operator = chooseOperation();
+            }
+
+            switch (operation) {
+                case '+':
+                    result = sum(firstNum, secondNum);
+                    break;
+                case '-':
+                    result = minus(firstNum, secondNum);
+                    break;
+                case '*':
+                    result = multiply(firstNum, secondNum);
+                    break;
+                case '/':
+                    result = divide(firstNum, secondNum);
+                    break;
+            }
+            System.out.println(firstNum + " " + operation + " " + secondNum + " = " + result + "\n");
+
+            System.out.println("Would you like to exit? y/n");
+
+            char exit = scanner.next().charAt(0);
+
+            while (exit != 'y' && exit != 'n') {
+                System.out.println("Write only y or n: ");
+                exit = scanner.next().charAt(0);
+            }
+
+            if (exit == 'y') {
                 break;
-            case '-':
-                result = minus(firstNum, secondNum);
-                break;
-            case '*':
-                result = multiply(firstNum, secondNum);
-                break;
-            case '/':
-                result = divide(firstNum, secondNum);
-                break;
+            }
         }
-        return firstNum + " " + operation + " " + secondNum + " = " + result + "\n";
     }
 
-    public void enterNumbers() {
+
+    public boolean enterNumbers() {
+        Scanner scanner = new Scanner(System.in);
         try {
             System.out.println("Enter 1st number: ");
             firstNum = scanner.nextDouble();
 
             System.out.println("Enter 2nd number: ");
             secondNum = scanner.nextDouble();
+            return false;
         } catch (InputMismatchException ime) {
-            throw new InputMismatchException("You cannot enter letters or special symbols.");
+            System.out.println("You cannot enter letters or special symbols.");
+            return true;
         }
     }
 
-    public char chooseOperation() {
+    public boolean chooseOperation() {
         System.out.println("Enter some operator:");
-
-        operation = scanner.next().charAt(0);
-
+        Scanner scanner = new Scanner(System.in);
         List<Character> validCharacters = List.of('+', '-', '*', '/');
 
-        boolean notValidCharacters = !(validCharacters.contains(operation));
-
-        if (notValidCharacters) {
-            throw new NoSuchElementException("Choose valid operation: +, -, *, /. You cannot use letters or special symbols.");
+        try {
+            operation = scanner.next().charAt(0);
+            if (!validCharacters.contains(operation)) {
+                throw new NoSuchElementException();
+            }
+            return false;
+        } catch (NoSuchElementException e) {
+            System.out.println("Choose valid operation: +, -, *, /. You cannot use letters or special symbols.");
+            return true;
         }
-        return operation;
     }
 }
